0eefe83cacaf2eb66cfb00c77a0d081373d3ae07
adelssss <166361844+adelssss@users.noreply.github.com>
2024-10-17 01:32:02 +0500

    blogModel.js

---
+++ blogModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const blogSchema= new mongoose.Schema({
+    title: {
+        type: String,
+        required: true, 
+    },
+    body: {
+        type: String,
+        required: true, 
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now, 
+    },
+})
+
+export default mongoose.model("blogs", blogSchema);
